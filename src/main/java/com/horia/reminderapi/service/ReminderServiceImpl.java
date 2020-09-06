f4a3ce5ac5cee6a6623e379ed12ba815cb7f6ea8
Horia <horiapavel@yahoo.com>
2020-09-06 21:14:12 +0300

    Rename reminder related classes

--- src/main/java/com/horia/reminderapi/service/ReminderServiceImpl.java
+++ src/main/java/com/horia/reminderapi/service/ReminderServiceImpl.java
@@ -3,7 +3,7 @@ package com.horia.reminderapi.service;
 import com.horia.reminderapi.exceptions.ResourceNotFoundException;
 import com.horia.reminderapi.model.Reminder;
 import com.horia.reminderapi.model.response.ApiResponse;
-import com.horia.reminderapi.repository.ReminderApiRepository;
+import com.horia.reminderapi.repository.ReminderRepository;
 import com.horia.reminderapi.sms.SleepingReminder;
 import com.horia.reminderapi.time.FutureTimeManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +11,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
-public class ReminderApiServiceImpl implements ReminderApiService {
+public class ReminderServiceImpl implements ReminderService {
 
     @Autowired
-    private ReminderApiRepository reminderRepository;
+    private ReminderRepository reminderRepository;
 
     private ThreadManager threadManager = new ThreadManager();
 
